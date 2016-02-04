98f1e25dd59d76e0f041d640b0b43e86d7516208
hanquez <hanquez@a10p13.fil.univ-lille1.fr>
2016-02-04 12:53:15 +0100

    Legere modification...

--- src/graphics/Fenetre.java
+++ src/graphics/Fenetre.java
@@ -3,18 +3,15 @@ package graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import game.Game;
-
 public class Fenetre extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private GamePanel gamePanel;
 	private Menu menu;
-	//private SelectLvl selectLvl;
 	private JPanel currentPanel;
 	
 	public Fenetre(){
 		this.setTitle("Mario Sukoban");
 		this.setResizable(false);
+		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		menu = new Menu(this);
 		this.currentPanel = menu;
